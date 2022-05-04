d63aa108ba42fe57b315c86a17bf06b9a968b2c4
LegenDNVF <asherchou@LegenDNVFs-MBP.home>
2022-05-04 00:57:05 -0700

    first commit

---
+++ main.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"reflect"
+	"strconv"
+
+	"github.com/pborman/uuid"
+	elastic "gopkg.in/olivere/elastic.v3"
+)
+
+const (
+	ES_URL   = "http://34.125.11.206:9200"
+	DISTANCE = "200km"
+	INDEX    = "around"
+	TYPE     = "post"
+)
+
+type Location struct {
+	Lat float64 `json:"lat"`
+	Lon float64 `json:"lon"`
+}
+
+type Post struct {
+	// `json:"user"` is for the json parsing of this User field. Otherwise, by default it's 'User'.
+	User     string   `json:"user"`
+	Message  string   `json:"message"`
+	Location Location `json:"location"`
+}
+
+func main() {
+	// Create a client
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	// Use the IndexExists service to check if a specified index exists.
+	exists, err := client.IndexExists(INDEX).Do()
+	if err != nil {
+		panic(err)
+	}
+	if !exists {
+		// Create a new index.
+		mapping := `{
+			"mappings":{
+				"post":{
+					"properties":{
+						"location":{
+							"type":"geo_point"
+						}
+					}
+				}
+			}
+		}`
+		_, err := client.CreateIndex(INDEX).Body(mapping).Do()
+		if err != nil {
+			// Handle error
+			panic(err)
+		}
+	}
+
+	fmt.Println("started-service")
+	http.HandleFunc("/post", handlerPost)
+	http.HandleFunc("/search", handlerSearch)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handlerSearch(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one request for search")
+	// lat,_ := r.URL.Query().Get("lat")
+	// lon,_ := r.URL.Query().Get("lon")
+	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
+	//range is optional
+	ran := DISTANCE
+	if val := r.URL.Query().Get("range"); val != "" {
+		ran = val + "km"
+	}
+
+	/*	fmt.Println("range is", ran)
+		//Return a fake post
+		p := &Post{
+			User:    "1111",
+			Message: "一生必去的100个地方",
+			Location: Location{
+				Lat: lat,
+				Lon: lon,
+			},
+		}*/
+
+	fmt.Printf("Search received: %f %f %s\n", lat, lon, ran)
+
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+	}
+
+	q := elastic.NewGeoDistanceQuery("location")
+	q = q.Distance(ran).Lat(lat).Lon(lon)
+
+	searchResult, err := client.Search().Index(INDEX).Query(q).Pretty(true).Do()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Query took %d milliseconds\n", searchResult.TookInMillis)
+	fmt.Printf("Found a total of %d posts\n", searchResult.TotalHits())
+
+	var typ Post
+	var ps []Post
+	for _, item := range searchResult.Each(reflect.TypeOf(typ)) {
+		// instance of
+		p := item.(Post) // p = (Post) item
+		fmt.Printf("Post by %s: %s at lat %v and lon %v\n", p.User, p.Message, p.Location.Lat, p.Location.Lon)
+		// TODO(student homework): Perform filtering based on keywords such as web spam etc.
+		ps = append(ps, p)
+
+	}
+
+	js, err := json.Marshal(ps)
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow_orgin", "*")
+	w.Write(js)
+	//	fmt.Fprint(w, "Search received: %s %s", lat, lon)
+}
+
+func handlerPost(w http.ResponseWriter, r *http.Request) {
+	// Parse from body of request to get a json object.
+	fmt.Println("Received one post request")
+	decoder := json.NewDecoder(r.Body)
+	var p Post
+
+	if err := decoder.Decode(&p); err != nil {
+		panic(err)
+		return
+	}
+	id := uuid.New()
+	// Save to ES.
+	saveToES(&p, id)
+
+	fmt.Fprintf(w, "Post received: %s\n", p.Message)
+
+}
+
+func saveToES(p *Post, id string) {
+	es_client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = es_client.Index().Index(INDEX).Type(TYPE).Id(id).BodyJson(p).Refresh(true).Do()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Print("Post is saved to index: %s\n", p.Message)
+
+}
---
+++ vimrc
@@ -0,0 +1,9 @@
+set nocompatible
+
+syntax on
+filetype indent plugin on
+
+set ruler
+
+set number
+
